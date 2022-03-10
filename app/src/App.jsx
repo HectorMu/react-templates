97ab989034a6f3b6a28cbe75c44f4c9358ea2cc0
HectorMu <skyspartanblack@gmail.com>
2022-03-09 19:24:17 -0600

    new full stack template with api included and with vite

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import Aos from "aos";
 import { useEffect } from "react";
-import "../node_modules/aos/dist/aos.css";
+import "../../node_modules/aos/dist/aos.css";
+import "../../node_modules/sweetalert2/dist/sweetalert2.css";
 import "./css/main.css";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
